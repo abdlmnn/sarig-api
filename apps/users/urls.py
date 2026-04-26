ea8a86b628e96144b033625e8da1bc0c4b8b4be5
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2026-04-26 18:34:44 +0800

    add user urls register login refresh

---
+++ apps/users/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path, include
+from .views import (
+  RegisterView,
+  MeView,
+)
+from rest_framework_simplejwt.views import (
+  TokenObtainPairView,
+  TokenRefreshView,
+)
+from .routers import router
+
+urlpatterns = [
+  path(
+    "register/",
+    RegisterView.as_view(),
+    name="register"
+  ),
+  path(
+    "login/",
+    TokenObtainPairView.as_view(),
+    name="token_obtain_pair"
+  ),
+  path(
+    "token/refresh/",
+    TokenRefreshView.as_view(),
+    name="token_refresh"
+  ),
+  path(
+    "",
+    include(router.urls)
+  ),
+]
+
