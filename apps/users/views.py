9a98428576a1d2925978dc1b60cc8a37049bd7a1
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2026-04-26 18:35:00 +0800

    add user views

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from rest_framework import (
+  generics,
+  permissions,
+  viewsets
+)
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .serializers import (
+  UserRegisterSerializer,
+  UserSerializer,
+  BarangaySerializer,
+  UserProfileSerializer,
+)
+from .models import (
+  User,
+  Barangay,
+  UserProfile
+)
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserRegisterSerializer
+  permission_classes = [permissions.AllowAny]
+
+class MeView(generics.RetrieveAPIView):
+  serializer_class = UserSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get_object(self):
+    return self.request.user
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+  queryset = UserProfile.objects.select_related('user', 'barangay')
+  serializer_class = UserProfileSerializer
+  permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+  def get_queryset(self):
+    qs = super().get_queryset()
+
+    role = self.request.query_params.get('role')
+    baranagay = self.request.query_params.get('barangay')
+    verification = self.request.query_params.get('verification_status')
+
+    if role:
+      qs = qs.filter(role=role)
+
+    if barangay:
+      qs = qs.filter(barangay_id=barangay)
+
+    if verification:
+      qs = qs.filter(verification_status=verification)
+
+    return qs
+
+  def perform_create(self, serializer):
+    if hasattr(self.request.user, 'profile'):
+      raise serializers.ValidationError('Profile already exists.')
+    serializer.save(user=self.request.user)
+
+  @action(
+    detail=False,
+    methods=['get'],
+    permission_classes=[permissions.IsAuthenticated]
+  )
+  def me(self, request):
+    profile = request.user.profile
+    serializer = self.get_serializer(profile)
+    return Response(serializer.data)
+
+class BarangayViewSet(viewsets.ReadOnlyModelViewSet):
+  queryset = Barangay.objects.all()
+  serializer_class = BarangaySerializer
+  permission_classes = [permissions.AllowAny]
