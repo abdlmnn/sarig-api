2c8c695d6a26a1fc8784f2cbbccc8c7bdb048201
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2026-05-05 03:51:33 +0800

    add v1 auth, users in urls

--- apps/v1/urls.py
+++ apps/v1/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = "v1"
 
 urlpatterns = [
-  path(
-    "users/",
-    include('apps.users.urls')
-  ),
+    path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("users/", include("apps.users.urls")),
 ]
-
