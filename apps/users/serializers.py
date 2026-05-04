27803ad48903a487fa24b47ec630cae40325f28e
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2026-05-05 03:55:47 +0800

    add serializers user, userregister, profile, address

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,72 +1,61 @@
 from rest_framework import serializers
-from .models import (
-  User,
-  UserProfile,
-  Barangay,
-)
+from .models import Role, User, Profile, Address
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-  password = serializers.CharField(
-    write_only=True,
-    required=True
-  )
-
-  class Meta:
-    model = User
-    fields = [
-      'id',
-      'username',
-      'email',
-      'password'
-    ]
-
-  def create(self, validated_data):
-    password = validated_data.pop('password')
-    user = User(**validated_data)
-    user.set_password(password)
-    user.save()
-    return user
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ("id", "username", "email", "phone_number", "roles")
 
-class BarangaySerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Barangay
-    fields = [
-      'id',
-      'name'
-    ]
 
-class UserProfileSerializer(serializers.ModelSerializer):
-  user = UserSerializer(
-    read_only=True
-  )
-  barangay = serializers.PrimaryKeyRelatedField(
-      queryset=Barangay.objects.all(),
-      write_only=True,
-      required=False
-  )
-  barangay_details = BarangaySerializer(
-      source='barangay',
-      read_only=True
-  )
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
-  class Meta:
-    model = UserProfile
-    fields = [
-      'id',
-      'user',
-      'role',
-      'phone_number',
-      'barangay',
-      'barangay_details',
-      'verification_status',
-      'created_at'
-    ]
-    read_only_fields = [
-      'created_at',
-      'verification_status'
-    ]
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "phone_number",
+            "password",
+        )
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            phone_number=validated_data["phone_number"],
+            password=validated_data["password"],
+        )
+
+        customer_role = Role.objects.get(name="customer")
+        user.roles.add(customer_role)
+
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = (
+            "id",
+            "user",
+            "avatar",
+            "date_of_birth",
+        )
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = (
+            "id",
+            "user",
+            "label",
+            "latitude",
+            "longitude",
+            "street_address",
+            "is_default",
+        )
