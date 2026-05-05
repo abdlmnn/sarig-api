e2f1c1b02b022cf25bbebac69f5c18b1e34736a2
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2026-05-06 03:23:50 +0800

    feat: Order-bound real-time chat with phone contact support

--- config/asgi.py
+++ config/asgi.py
@@ -2,7 +2,8 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from apps.orders.routing import websocket_urlpatterns
+from apps.orders.routing import websocket_urlpatterns as order_ws
+from apps.chat.routing import websocket_urlpatterns as chat_ws
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
 
@@ -10,7 +11,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns
+            order_ws + chat_ws
         )
     ),
 })
