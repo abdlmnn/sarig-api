e0269c410e766ff3e914fdc7ce36c5051d6f528a
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2026-05-06 03:24:29 +0800

    feat: Global product search and smart price comparison engine

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet
+from .views import CategoryViewSet, ProductViewSet, GlobalProductSearchView, ProductComparisonView
 
 router = DefaultRouter()
 router.register(r"categories", CategoryViewSet)
@@ -8,4 +8,6 @@ router.register(r"products", ProductViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("search/", GlobalProductSearchView.as_view(), name="global-search"),
+    path("compare/", ProductComparisonView.as_view(), name="product-compare"),
 ]
