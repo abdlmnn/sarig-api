da619b4371389ed4c41ccd7d39ae74bd42deacb0
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2026-08-14 16:05:54 +0800

    add shared haversine_km distance helper

--- apps/users/geo.py
+++ apps/users/geo.py
@@ -1,4 +1,16 @@
 from decimal import Decimal
+from math import asin, cos, radians, sin, sqrt
+
+EARTH_RADIUS_KM = 6371.0
+
+
+def haversine_km(lon1, lat1, lon2, lat2):
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    return EARTH_RADIUS_KM * c
 
 
 def get_lat_lng(obj, lat_attr="latitude", lng_attr="longitude"):
